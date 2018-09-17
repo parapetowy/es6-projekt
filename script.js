ace31a356c3eb92f4a416d5a097ae4a591756a0e
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-09-17 21:05:05 +0200

    fixed

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
 class App extends React.Component {
   constructor() {
     super();
@@ -66,3 +61,8 @@ class User extends React.Component {
     );
   }
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
